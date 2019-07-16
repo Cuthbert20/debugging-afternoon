95cd07afce7b4509002f9ef460f1da59bc3404f8
Cuthbert20 <knowltonspencer@gmail.com>
2019-07-16 14:29:57 -0600

    finished

--- stage-2/src/App.js
+++ stage-2/src/App.js
@@ -5,8 +5,8 @@ import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
 import NavBar from "./Components/NavBar/NavBar";
 
 class App extends Component {
-  constuctor(props) {
-    super(props);
+  constructor(props) {
+    super(props)
     this.state = {
       products: [],
       cart: [],
@@ -20,19 +20,23 @@ class App extends Component {
     axios
       .get("https://practiceapi.devmountain.com/products/")
       .then(response => {
+        //console.log(response.data)
         this.setState({
-          products: response
+          products: response.data
         });
       });
   }
   addToCart(item) {
     this.setState({
-      cart: item
+      cart: [...this.state.cart, item]
     });
   }
   removeFromCart(index) {
-    let cartCopy = this.state.products.slice();
+    console.log(index)
+    let cartCopy = this.state.cart.slice();
+    //console.log(cartCopy)
     cartCopy.splice(index, 1);
+    console.log(cartCopy)
     this.setState({
       cart: cartCopy
     });
